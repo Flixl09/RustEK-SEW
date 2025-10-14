c4d8158dffeca099e5803605a6cfcd8ed4e9d002
Felix Schmid <felix20.09@hotmail.com>
2025-10-14 15:42:04 +0200

    init all other

---
+++ tests/basics.rs
@@ -0,0 +1,9 @@
+use rust_ek::*;
+
+#[test]
+fn basics_work() {
+    assert_eq!(greet("Ada"), "Hello, Ada!");
+    assert_eq!(sum(&[1, 2, 3, 4]), 10);
+    assert_eq!(flip(true), false);
+    assert_eq!(flip(false), true);
+}
